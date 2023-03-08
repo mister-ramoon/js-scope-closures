0149357bf2fb91e8eb8f8357def5f2c1da89a282
ramonruiz <jruiz@lightia.mx>
2023-03-08 09:46:12 -0600

    Reasignación y redeclaración

---
+++ src/scope/re.js
@@ -0,0 +1,27 @@
+var firstName; // Undefined
+firstName = "Juan";
+console.log(firstName);
+
+var lastName = "David"; // Declarar y Asignar
+lastName = "Ana"; // Reasingar
+console.log(lastName);
+
+var secondName = "David"; // Declarar y Asignar
+var secondName = "Ana"; // Reasingar y redeclarar
+console.log(secondName);
+
+// Let
+let fruit = "Apple"; // Declarar y Asignar
+fruit = "Kiwi"; // Reasingar
+console.log(fruit);
+
+// Const
+const animal = "perrito";
+console.log(animal);
+
+const vehicles = [];
+vehicles.push("🚗");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
