3f9bbe9c21d7344a790e8fe838ce7e00319644c2
ramonruiz <jruiz@lightia.mx>
2023-03-13 14:02:25 -0600

    Hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,14 @@
+var nameOfdDog; // undefined
+
+console.log(nameOfdDog);
+var nameOfdDog = "Elmo";
+console.log(nameOfdDog);
+
+var elmo; // undefined
+nameDog();
+
+function nameDog() {
+  console.log(`El mejor perro es ${elmo}`);
+}
+
+var elmo = "Elmo";
